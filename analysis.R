da5a30ac8375700f43c113db2f9e3967ae751ee8
kancheng <zxdfgcv@gmail.com>
2019-07-14 06:18:17 +0800

    edit

--- analysis.R
+++ analysis.R
@@ -195,29 +195,29 @@ filter.plot = function( data, main.name) {
 
 }
 
-# 表格自動輸出至工作目錄
-
-# 繪圖自動輸出至工作目錄
-
 # 中國經濟分析 ALL
+
+cn.mac = function() {
+
+}
 
 
 ######
 
 # 時間序列 PIC
-par(mfrow=c(1,1))
-plot(cn.gdp.1y.ts)
+# par(mfrow=c(1,1))
+# plot(cn.gdp.1y.ts)
 
 
 # 檔科學記號
 # options(scipen=999)
 
-opar <- par(no.readonly=TRUE)
-par(mfrow=c(1,1),mar=c(3,3,2,1))
-plot(cn.gdp.1y.ts,main="1953 - 2018 國民總收入 - 億元",col=1,ylab="")
-lines(cn.gdp.1y.ts.hp$trend,col=2)
-lines(cn.gdp.1y.ts.bk$trend,col=3)
-lines(cn.gdp.1y.ts.cf$trend,col=4)
-lines(cn.gdp.1y.ts.bw$trend,col=5)
-lines(cn.gdp.1y.ts.tr$trend,col=6)
-legend("topleft",legend=c("series", "HP","BK","CF","BW", "TR"),col=1:6, lty=rep(1,6),ncol=2)
+# opar <- par(no.readonly=TRUE)
+# par(mfrow=c(1,1),mar=c(3,3,2,1))
+# plot(cn.gdp.1y.ts,main="1953 - 2018 國民總收入 - 億元",col=1,ylab="")
+# lines(cn.gdp.1y.ts.hp$trend,col=2)
+# lines(cn.gdp.1y.ts.bk$trend,col=3)
+# lines(cn.gdp.1y.ts.cf$trend,col=4)
+# lines(cn.gdp.1y.ts.bw$trend,col=5)
+# lines(cn.gdp.1y.ts.tr$trend,col=6)
+# legend("topleft",legend=c("series", "HP","BK","CF","BW", "TR"),col=1:6, lty=rep(1,6),ncol=2)
